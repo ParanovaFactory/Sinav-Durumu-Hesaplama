11d512a205fa304139e50251f3a30896a6e3d683
Berkay <bkaraduman64@gmail.com>
2020-12-27 18:48:34 +0300

    Sınav Durumu Hesaplama

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınav_Durumu_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double snv1, snv2, snv3, ort;
+            snv1 = Convert.ToDouble(textBox3.Text);
+            snv2 = Convert.ToDouble(textBox4.Text);
+            snv3 = Convert.ToDouble(textBox5.Text);
+            ort = (snv1 + snv2 + snv3) / 3;
+            ortlabel.Text = ort.ToString(0.00);
+            listBox1.Items.Add("Ad: " + textBox1.Text);
+            listBox1.Items.Add("Soyad: " + textBox2.Text);
+            if (ort >= 60) 
+            {
+                listBox1.Items.Add("Dersten Geçtiniz");
+            }
+            else
+            {
+                listBox1.Items.Add("Dersten Kaldınız");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Sinav-Durumu-Hesaplama
+# Sınav Durumu Hesaplama
+# Sisteme girilen notları herhangi bir kat sayı çarpımına oğratmadan doğrudan hesaplama yapar sonrasında ad soyad bilgisi ile birlikte geçip kalma durumunu listbox'a yazar
